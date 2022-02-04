bcb8b71c41c3a0df2d599779d933fcf7d9ec85e7
sahithi-code <71334668+sahithi-code@users.noreply.github.com>
2022-02-04 11:56:28 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,15 @@
+print("--MY PROFILE---")
+
+name = input("enter your name: ")
+clAss = input("enter your class which studing(in numerals): ")
+schoolName = input("enter you school name: ")
+age = input("How old are you : ")
+parents = input("write your parentNames: ")
+
+print("--------")
+print("My name is",name)
+print("Im studing in",clAss,"th class.")
+print("My schoolName is",schoolName)
+print("I'm",age,"old")
+print("I'm blessed kid of",parents)
+
